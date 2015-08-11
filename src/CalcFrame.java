4cef5783e286c0c271ae0d8516e228b48f24063e
p5ycod3 <8bit.ds@gmail.com>
2015-08-11 20:54:08 +0300

    replace with lambda

--- src/CalcFrame.java
+++ src/CalcFrame.java
@@ -1,7 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
 
 public class CalcFrame extends JFrame {
     CalcFrame() {
@@ -29,128 +29,60 @@ public class CalcFrame extends JFrame {
         final String[] operation = {"+"};
 
 
-        button0.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                display.setText(display.getText() + "0");
-            }
-        });
-        button1.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                display.setText(display.getText() + "1");
-            }
-        });
-        button2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                display.setText(display.getText() + "2");
-            }
-        });
-        button3.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                display.setText(display.getText() + "3");
-            }
-        });
-        button4.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                display.setText(display.getText() + "4");
-            }
-        });
-        button5.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                display.setText(display.getText() + "5");
-            }
-        });
-        button6.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                display.setText(display.getText() + "6");
-            }
-        });
-        button7.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                display.setText(display.getText() + "7");
-            }
-        });
-        button8.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                display.setText(display.getText() + "8");
-            }
-        });
-        button9.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                display.setText(display.getText() + "9");
-            }
+        button0.addActionListener(e -> display.setText(display.getText() + "0"));
+        button1.addActionListener(e -> display.setText(display.getText() + "1"));
+        button2.addActionListener(e -> display.setText(display.getText() + "2"));
+        button3.addActionListener(e -> display.setText(display.getText() + "3"));
+        button4.addActionListener(e -> display.setText(display.getText() + "4"));
+        button5.addActionListener(e -> display.setText(display.getText() + "5"));
+        button6.addActionListener(e -> display.setText(display.getText() + "6"));
+        button7.addActionListener(e -> display.setText(display.getText() + "7"));
+        button8.addActionListener(e -> display.setText(display.getText() + "8"));
+        button9.addActionListener(e -> display.setText(display.getText() + "9"));
+
+
+        buttonC.addActionListener(e -> {
+            String temp = display.getText();
+            display.setText(temp.substring(0, temp.length() - 1));
         });
 
 
-        buttonC.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                String temp = display.getText();
-                display.setText(temp.substring(0, temp.length() - 1));
-            }
+        buttonPlus.addActionListener(e -> {
+            firstValue[0] = Integer.valueOf(display.getText());
+            display.setText("");
+            operation[0] = "+";
         });
-
-
-        buttonPlus.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                firstValue[0] = Integer.valueOf(display.getText());
-                display.setText("");
-                operation[0] = "+";
-            }
+        buttonMul.addActionListener(e -> {
+            firstValue[0] = Integer.valueOf(display.getText());
+            display.setText("");
+            operation[0] = "*";
         });
-        buttonMul.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                firstValue[0] = Integer.valueOf(display.getText());
-                display.setText("");
-                operation[0] = "*";
-            }
+        buttonDivide.addActionListener(e -> {
+            firstValue[0] = Integer.valueOf(display.getText());
+            display.setText("");
+            operation[0] = "/";
         });
-        buttonDivide.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                firstValue[0] = Integer.valueOf(display.getText());
-                display.setText("");
-                operation[0] = "/";
-            }
+        buttonMinus.addActionListener(e -> {
+            firstValue[0] = Integer.valueOf(display.getText());
+            display.setText("");
+            operation[0] = "-";
         });
-        buttonMinus.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                firstValue[0] = Integer.valueOf(display.getText());
-                display.setText("");
-                operation[0] = "-";
+        buttonStart.addActionListener(e -> {
+            int secondValue = Integer.valueOf(display.getText());
+            if ("+".equals(operation[0])) {
+                display.setText((firstValue[0] + secondValue) + "");
             }
-        });
-        buttonStart.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                int secondValue = Integer.valueOf(display.getText());
-                if ("+".equals(operation[0])) {
-                    display.setText((firstValue[0] + secondValue) + "");
-                }
-                if ("-".equals(operation[0])) {
-                    display.setText((firstValue[0] - secondValue) + "");
-                }
-                if ("*".equals(operation[0])) {
-                    display.setText((firstValue[0] * secondValue) + "");
-                }
-                if ("/".equals(operation[0])) {
-                    display.setText((firstValue[0] / secondValue) + "");
-                }
-                firstValue[0] = 0;
-                operation[0] = "+";
+            if ("-".equals(operation[0])) {
+                display.setText((firstValue[0] - secondValue) + "");
             }
+            if ("*".equals(operation[0])) {
+                display.setText((firstValue[0] * secondValue) + "");
+            }
+            if ("/".equals(operation[0])) {
+                display.setText((firstValue[0] / secondValue) + "");
+            }
+            firstValue[0] = 0;
+            operation[0] = "+";
         });
         setBounds(300, 300, 300, 300);
         setLayout(new BorderLayout());
